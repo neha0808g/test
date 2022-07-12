3ed5cf02fb96098567e598e9117bcb5863e1b421
neha0808g <109165766+neha0808g@users.noreply.github.com>
2022-07-12 21:52:58 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,28 @@
+def convert():
+    number = int(input("Enter a number: "))
+
+    if number % 3 == 0 and number % 5 == 0 and number % 7 == 0:
+        print("PlingPlangPlong")
+
+    elif number % 3 == 0 and number % 5 == 0:
+        print("PlingPlang")
+
+    elif number % 5 == 0 and number % 7 == 0:
+        print("PlangPlong")
+
+    elif number % 3 == 0 and number % 7 == 0:
+        print("PlingPlong")
+
+    elif number % 3 == 0:
+        print("Pling")
+
+    elif number % 5 == 0:
+        print("Plang")
+
+    elif number % 7 == 0:
+        print("Plong")
+
+    else:
+        print(number)
+
+convert()
